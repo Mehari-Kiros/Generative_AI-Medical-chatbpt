cf935dec07dc96ee077aacdae2c96d7ce286ff7f
Mehari Kiros <meharik1219@gmail.com>
2025-03-14 11:34:50 +0300

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Medical recommender',
+    version= '0.0.0',
+    author= 'Mehari-Kiros',
+    author_email= 'meharik1219@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
